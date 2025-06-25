27e56859abb58bc4478133d6e519679b178acbe1
Gaugo47 <defoygauthier@gmail.com>
2025-06-25 16:55:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISPLAY=:0
 log_file="/home/admin/service_log.txt"
-cd /home/admin/Documents/webrtc_screen_share
+cd /home/admin/SmartScreen
 npm run start >> "$log_file" 2>&1  &
 chromium-browser --kiosk "https://cast.davincibot.fr/public/receiver.html" >> "$log_file" 2>&1 & 
